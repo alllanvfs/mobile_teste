a5bfe60d51e45c6c229b9084ea5c3ea235d0c09a
Allan Santos <101256251+alllanvfs@users.noreply.github.com>
2025-11-08 13:24:13 -0300

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+
+    alias(libs.plugins.android.application) apply false
+}
